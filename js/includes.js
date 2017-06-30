4a4c26d29b306063de5e9d883f38ae443eb9ecae
anonimezhuk <polchanov.alex@gmail.com>
2017-06-30 20:39:11 +0300

    first
    first

--- js/includes.js
+++ js/includes.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
+  $('body').css(
+    'height', $(window).height()-20+'px'
+  );
+  var prevColor = '#ffffff';
   $('.slider').slick({
     slidesToShow: 1,
     autoplay: false,
     arrows: false,
     infinite: false
+  });
+  $('.slider').on('swipe', function(){
+    var color = $('.slick-active').data('color');
+    console.log(prevColor);
+    $('body').css(
+      'background', 
+      "linear-gradient(to top left, "+prevColor+","+color+") no-repeat",
+      
+    );
+    prevColor = color;
+    console.log(color);
   });
 });
